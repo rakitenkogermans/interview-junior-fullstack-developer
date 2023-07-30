f398e2fa5b2f0b98339c408126a2a58696e0aa87
rakitenkogermans <rakitenko.germans@gmail.com>
2023-07-30 18:27:00 +0300

    enable cors in development mode

--- interview-backend/src/main.ts
+++ interview-backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (process.env.NODE_ENV !== 'production') {
+    app.enableCors();
+  }
   await app.listen(3000);
 }
 bootstrap();
